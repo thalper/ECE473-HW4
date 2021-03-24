817ed1a4fa4262d8808483af1967549d50f26e9e
Tycho <thalper@purdueedu>
2021-03-24 12:27:13 -0400

    Fished  homework assignment

--- hw4_submission.py
+++ hw4_submission.py
@@ -12,17 +12,26 @@ class SegmentationProblem(util.SearchProblem):
 
     def start(self):
         # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
+        return 0
+        # raise Exception("Not implemented yet")
         # END_YOUR_CODE
 
     def goalp(self, state):
         # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
+        return len(self.query) == state
+        # raise Exception("Not implemented yet")
         # END_YOUR_CODE
 
     def expand(self, state):
         # BEGIN_YOUR_CODE (our solution is 5 lines of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
+        expList = []
+        for i in range(state,len(self.query)):
+            action = self.query[state:i+1]
+            newState = i + 1
+            cost = self.unigramCost(action)
+            expTuple = (action, newState, cost)
+            expList.append(expTuple)
+        return expList
         # END_YOUR_CODE
 
 def segmentWords(query, unigramCost):
@@ -33,7 +42,7 @@ def segmentWords(query, unigramCost):
     ucs.solve(SegmentationProblem(query, unigramCost))
 
     # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    return ' '.join(ucs.actions)
     # END_YOUR_CODE
 
 ############################################################
@@ -47,22 +56,45 @@ class VowelInsertionProblem(util.SearchProblem):
 
     def start(self):
         # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
+        return (wordsegUtil.SENTENCE_BEGIN, 0)
         # END_YOUR_CODE
 
     def goalp(self, state):
         # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
+        return state[1] == len(self.queryWords) - 1
         # END_YOUR_CODE
 
     def expand(self, state):
         # BEGIN_YOUR_CODE (our solution is 6 lines of code, but don't worry if you deviate from this)
-        raise Exception("Not implemented yet")
+        expList = []
+        PF = []
+        word = self.queryWords[state[1] + 1]
+        PF = self.possibleFills(word)
+        if len(PF) == 0:
+            action = word
+            newState = (action, state[1] + 1)
+            cost = self.bigramCost(state[0], action)
+            expTuple = (action, newState, cost)
+            expList.append(expTuple)
+        for y in PF:
+            cost = self.bigramCost(state[0], y)
+            action = y
+            newState = (action, state[1] + 1)
+            expTuple = (action, newState, cost)
+            expList.append(expTuple)
+                    
+        return expList
         # END_YOUR_CODE
 
 def insertVowels(queryWords, bigramCost, possibleFills):
     # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    if len(queryWords) == 0:
+        return ''
+    queryWords.insert(0, wordsegUtil.SENTENCE_BEGIN)
+    bcs = util.UniformCostSearch(verbose=0)
+    bcs.solve(VowelInsertionProblem(queryWords, bigramCost, possibleFills))
+
+    return ' '.join(bcs.actions)
     # END_YOUR_CODE
 
 
